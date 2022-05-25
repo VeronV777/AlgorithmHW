7ecf02bc49db4ee074c45b768deb3dbd75303429
VeronikaVl <vlveronika61@gmail.com>
2022-05-25 17:55:39 -0400

    add exstra spaces in if statement

--- src/main/java/IsPositiveNumber.java
+++ src/main/java/IsPositiveNumber.java
@@ -2,7 +2,7 @@ public class IsPositiveNumber {
     //Напишите алгоритм который возвращает true, если численный
     // параметр больше или равен нулю, и возвращает false, если параметр меньше 0.
     public boolean IsPositiveNumberAlgorithm (int first){
-        if (first>=0){
+        if (first >= 0){
             return true;
 
         } else
